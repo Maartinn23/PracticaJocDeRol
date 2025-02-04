4a60a716fe56ea0aa043b5ed084f82c510b1c67f
martinn23 <dfmartinn.23@gmail.com>
2025-02-04 15:39:42 +0100

    prueba

--- src/practicagestorjocrolmartin/PracticaGestorJocRolMartin.java
+++ src/practicagestorjocrolmartin/PracticaGestorJocRolMartin.java
@@ -15,6 +15,9 @@ public class PracticaGestorJocRolMartin {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        
+        System.out.println("Hello world!");
     }
     
 }
